3a20689373264b9501e1acd4ee026ea5bae400bc
Eleven <eleveneat@gmail.com>
2014-12-15 20:07:23 +0800

    Lab5 final version.

---
+++ web2.0/Ex9/Lab5/README.md
@@ -0,0 +1,2 @@
+#Reference
+[Lab 5 JavaScript for Interactive User Interfaces(Chinese)](http://my.ss.sysu.edu.cn/wiki/pages/viewpage.action?pageId=76906540)
--- web2.0/Ex9/Lab5/pimpmytext.js
+++ web2.0/Ex9/Lab5/pimpmytext.js
@@ -1,14 +1,25 @@
+/*
+*    Filename: pimpmytext.js
+*    Description: Lab5.
+*    Last modified: 2014-12-15 19:58
+*
+*    Created by Eleven on 2014-12-15
+*    Email: eleveneat@gmail.com
+*    Copyright (C) 2014 Eleven. All rights reserved.
+*/
+
 $ = function(obj) {
 	return document.getElementById(obj);
 }
 
 window.onload = function() {
-	// alert("1");
-	$("BiggerPimpin").onclick = enlargeFont;
+	//增加事件处理器
+	$("BiggerPimpin").addEventListener('click', enlargeFont);
 	$("Bling").addEventListener('click', changeFontStyle);
 	$("Snoopify").addEventListener('click', upperCaseAndSuffixSentence);
 }
 
+//增加字体大小，直接用style是不好的行为，谨记
 var fontSize = 12;
 function enlargeFont() {
 	fontSize += 2;
@@ -16,6 +27,7 @@ function enlargeFont() {
 	document.getElementsByTagName("body")[0].style.backgroundImage = "url(hundred-dollar-bill.jpg)";
 }
 
+//改变样式
 function changeFontStyle() {
 	if($("Bling").checked) {
 		$("Text").className = "styleText";
@@ -24,6 +36,7 @@ function changeFontStyle() {
 	}
 }
 
+//在每个句子后面增加后缀"-izzle"。（一个充当文本字符串的句子以句号字符"."结尾）
 function upperCaseAndSuffixSentence() {
 	var str = $("Text").value;
 	var parts = str.toUpperCase().split(".");
